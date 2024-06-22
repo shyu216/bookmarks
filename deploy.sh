24c8d51fd81b8a58953d4bff9b0d9ff32014d74e
shyu216 <sihong1@student.unimelb.edu.au>
2024-06-22 20:44:20 +1000

    test

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:shyu216/a-bookmark.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:shyu216/a-bookmark.git main:gh-pages
 
 cd -
