f0b4b9abf0873706b3c4844c4992559ca2285174
shyu216 <sihong1@student.unimelb.edu.au>
2024-06-22 20:17:16 +1000

    add dist

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:shyu216/a-bookmark.github.io.git main
+git push -f git@github.com:shyu216/a-bookmark.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
