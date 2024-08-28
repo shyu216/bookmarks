9b6748836612442a5d391a861600308e29a80acb
shyu216 <yusihong073@gmail.com>
2024-08-28 20:57:13 +1000

    deploy

--- deploy.bat
+++ deploy.bat
@@ -18,6 +18,6 @@ git add -A
 git commit -m "deploy"
 
 REM if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shyu216/a-bookmark.git main:gh-pages
+git push -f git@github.com:shyu216/bookmarks.git main:gh-pages
 
 cd ..
--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shyu216/a-bookmark.git main:gh-pages
+git push -f git@github.com:shyu216/bookmarks.git main:gh-pages
 
 cd -
